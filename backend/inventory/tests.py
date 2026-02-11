0d506439bbd67d8bf9aa38ff7a787c5c1382a4ed
Kelvin Kitheka <kelvinkitheka10@gmail.com>
2026-02-11 20:32:48 +0300

    Add tests to ensure product is created correctly

--- backend/inventory/tests.py
+++ backend/inventory/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Product
+from decimal import Decimal
 
 # Create your tests here.
+class ProductModelTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username = 'testuser',
+            password = 'testpass123'
+        )
+
+        self.product = Product.objects.create(
+            user = self.user,
+            name = 'Milk',
+            category = 'Dairy',
+            default_sell_price = Decimal('120.00')
+        )
+
+    def test_product_creation(self):
+        self.assertEqual(self.product.name, 'Milk')
+        self.assertEqual(self.product.category, 'Dairy')
+        self.assertTrue(self.product.is_active)
+
+    
+    
+
+        
