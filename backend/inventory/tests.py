ed9f60b33baa5b4608f6e9df9756de3cea0d0295
Kelvin Kitheka <kelvinkitheka10@gmail.com>
2026-02-18 20:49:48 +0300

    Add product creation test

--- backend/inventory/tests.py
+++ backend/inventory/tests.py
@@ -5,6 +5,7 @@ from decimal import Decimal
 from django.utils import timezone
 from datetime import timedelta
 from rest_framework.test import APITestCase, APIClient
+from rest_framework import status
 # Create your tests here.
 class ProductModelTest(TestCase):
     def setUp(self):
@@ -211,7 +212,7 @@ class LowStockAlertModelTest(TestCase):
 
 #Add ProductAPI tests
 class ProductAPITest(APITestCase):
-    def setup(self):
+    def setUp(self):
         self.user = User.objects.create_user(
             username = 'testpass',
             password = 'testuser123'
@@ -222,9 +223,17 @@ class ProductAPITest(APITestCase):
 
         self.product_data = {
             'name' : 'Milk',
-            'category' : 'Food',
+            'category' : 'food',
             'default_sell_price' : '60'
          }
+        
+
+    def test_create_product(self):
+        response = self.client.post('/api/products/', self.product_data)
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Product.objects.count(), 1)
+        self.assertEqual(Product.objects.get().name, 'Milk')
 
 
 
